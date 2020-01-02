7690f4494374964914b9fd15aa1a19eb76f9999f
Xavier Carty <xaviercarty@gmail.com>
2020-01-02 06:07:07 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
 # Your Code Here
+def map(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    
+  
+  new_array.push((source_array[i] * -1))
+  i += 1 
+  
+ end
+ base = "#{new_array}"
+ 
+  new_array
+
+
+
+
+ map([1,2,3,4])
